a905767ef246a5170f8957edb5dcff5badd02e47
ramzi-omari <ramziomari1995@gmail.com>
2021-01-07 17:48:52 +0100

    Banner first part

--- airbnbclonereact/src/App.js
+++ airbnbclonereact/src/App.js
@@ -5,8 +5,8 @@ import Home from "./Home";
 function App() {
   return (
     <div className="app">
-      <h1>lol</h1>
       <Header></Header>
+      <Home></Home>
       {/*1 Home */}
       {/* 2header */}
 
--- airbnbclonereact/src/Home.js
+++ airbnbclonereact/src/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Banner from "./Banner";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
-      <h1> Home</h1>
+      <Banner></Banner>
     </div>
   );
 }
