658521f9d9361c3f92191e39c42033a4490c5eaa
ramzi-omari <ramziomari1995@gmail.com>
2021-01-08 02:26:26 +0100

    Search Result component Styled

--- airbnbclonereact/src/SearchPage.js
+++ airbnbclonereact/src/SearchPage.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./SearchPage.css";
+import { Button } from "@material-ui/core";
+import SearchResult from "./SearchResult";
 
 function SearchPage() {
   return (
-    <div>
-      <h1>searchp</h1>
+    <div className="searchPage">
+      <div className="searchPage__info">
+        <p>62 stays . 26 august to 30 august . 2 guest</p>
+        <h1>Stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
+      </div>
+
+      <SearchResult
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&uspq=CAU"
+        location="Private room in center of London"
+        title="Stay at this spacious Edwardian House"
+        description="1 guest . 1 bedroom . 1 bed . 1.5 shared bthrooms . Wifi . Kitchen . Free parking . Washing MAchine"
+        star={4.73}
+        price="$30 / night"
+        total="£117 total"
+      ></SearchResult>
     </div>
   );
 }
